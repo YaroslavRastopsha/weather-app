3dc6c9b981745b2912d2f90b5e1700110dc3ff41
YaroslavRastopsha <rastopsha.yaroslav@student.sumdu.edu.ua>
2026-03-29 13:43:10 +0300

    add FormsModule and ngModel search input

--- src/app/features/weather-list/weather-list.ts
+++ src/app/features/weather-list/weather-list.ts
@@ -1,18 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { WeatherCard } from '../../shared/components/weather-card/weather-card';
 import { WEATHER_ITEMS } from '../../shared/mock-data';
-import { WeatherItem } from '../../shared/models/weather-item';
+import { WeatherCondition, WeatherItem } from '../../shared/models/weather-item';
 
 @Component({
   selector: 'weather-list',
-  imports: [WeatherCard],
+  imports: [WeatherCard, FormsModule],
   templateUrl: './weather-list.html',
   styleUrl: './weather-list.css',
 })
 export class WeatherList {
-  public items: WeatherItem[] = WEATHER_ITEMS;
+  public searchQuery: string = '';
+  public selectedCondition: string = 'All';
+
+  public allProducts: WeatherItem[] = WEATHER_ITEMS;
+  public filteredProducts: WeatherItem[] = WEATHER_ITEMS;
+
+  public conditionOptions: string[] = [
+    'All',
+    ...Object.values(WeatherCondition),
+  ];
 
   handleCardAction(id: number): void {
     console.log(`Користувач натиснув кнопку на товарі з ID: ${id}`);
+  }
+
+  filterItems(): void {
+    const query = this.searchQuery.toLowerCase().trim();
+
+    this.filteredProducts = this.allProducts.filter((item) => {
+      const matchesText = item.title.toLowerCase().includes(query);
+      const matchesCondition =
+        this.selectedCondition === 'All' ||
+        item.condition === this.selectedCondition;
+
+      return matchesText && matchesCondition;
+    });
+  }
+
+  resetFilters(element: HTMLInputElement): void {
+    this.searchQuery = '';
+    this.selectedCondition = 'All';
+    this.filterItems();
+    element.focus();
   }
 }
